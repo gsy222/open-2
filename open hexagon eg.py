bcd5eea7ee9d3a7fba8c44ff6db945d0e197fe83
fujiang520 <m-13181588@moe-dl.edu.my>
2024-09-20 19:08:32 +0800

    Update open hexagon eg.py

--- open hexagon eg.py
+++ open hexagon eg.py
@@ -6,15 +6,7 @@ def popup():
     x = random.randint(100, 700)  # 随机X坐标
     y = random.randint(100, 500)  # 随机Y坐标
     root.geometry(f"+{x}+{y}")    # 移动窗口到随机位置
-    result = messagebox.askquestion("", "叫我一声爸")  # 显示“是”或“不是”
-    
-    if result == 'yes':  # 用户选择 "是"
-        messagebox.showinfo("", "哎乖")  # 显示一个新消息
-        roop.quit(10)  #退出程序
-    else:  # 用户选择 "不是"
-        messagebox.showinfo("", "好吧！")  # 显示另一个消息
-    
-    messagebox.showinfo("", "好吧！哈哈哈")  # 再次显示原来的消息
+    messagebox.showinfo("Hi!", "I`m hack your computer now!")
 
 root = tk.Tk()
 root.withdraw()  # 隐藏主窗口
